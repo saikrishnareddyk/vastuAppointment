e644d791c3fd81a5f5575538b5ac5842988b7ba5
krishna reddy <kreddy@efficiencyware.com>
2026-05-20 12:32:14 +0530

    model with validations.  min validation

---
+++ VastuBookingApi/Models/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VastuBookingApi.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(10)]
+        public string MobileNumber { get; set; } = string.Empty;
+
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+    }
+}
