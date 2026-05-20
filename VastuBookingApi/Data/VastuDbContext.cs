e27cefe3e9d904c791319845be609c5b7f87e29d
krishna reddy <kreddy@efficiencyware.com>
2026-05-20 12:33:00 +0530

    db context

---
+++ VastuBookingApi/Data/VastuDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VastuBookingApi.Models;
+
+namespace VastuBookingApi.Data
+{
+    public class VastuDbContext : DbContext
+    {
+        public VastuDbContext(DbContextOptions<VastuDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
